8baa6c7313da3be83ee850d830d091dff3c7e17a
jbradley84 <utbradley84@yahoo.com>
2022-06-03 11:07:01 -0500

    created if statement to check hour id against current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+elEightAm = document.querySelector("#time8");
+elNineAm = document.querySelector("#time9"); 
+elTenAm = document.querySelector("#time10"); 
+elElevenAm = document.querySelector("#time11");
+elTwelvePm = document.querySelector("#time12");
+elOnePm = document.querySelector("#time13");
+elTwoPm = document.querySelector("#time14");
+elThreePm = document.querySelector("#time15");
+elFourPm = document.querySelector("#time16");
+elFivePm = document.querySelector("#time17");
+
 var printDate = function () {
     // format date into string
     var calendarDate = moment().format("dddd" + ", " + "MMMM" + " Do");
@@ -14,10 +25,10 @@ var printDate = function () {
 //     $(input.id).addClass("past");
 // }
 
-console.log(document.querySelector("input").id);
+console.log(document.querySelector("input").id.slice(4));
 console.log(moment().format("H"));
 
-if (document.querySelector("input").id < moment().format("H")) {
+if (parseInt(document.querySelector("input").id.slice(4)) < parseInt(moment().format("H"))) {
     console.log("you're onto something");
 } else {
     console.log("try again");
